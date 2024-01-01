e69d7d73c96c90d569ea5dae45d723a580262fd8
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 08:53:37 -0800

    done with the delete function

--- src/components/DeleteProduct.js
+++ src/components/DeleteProduct.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const DeleteProduct = () => {
+  const [inputValue , setInputValue] = useState('');
+
+
+
+  const deleteData = (event) => {
+    event.preventDefault();
+    axios.delete(`products/${inputValue}`).then().catch()
+
+  }
+
+
+
   return (
     <div style={{
       display: 'flex',
@@ -9,9 +22,13 @@ const DeleteProduct = () => {
       rowGap: '20px'
     }}>
       <h3>Delete a Product</h3>
-      <form action="">
-        <input type="number" placeholder='Type Id...'name='id' />
-        <button type='submit'>Delete Product</button>
+      <form onSubmit={deleteData}>
+        <input value={inputValue} onChange={event => setInputValue(event.target.value)} type="number" placeholder='Type Id...'name='id' />
+
+        {
+          inputValue &&
+          <button type='submit'>Delete Product</button>
+        }
       </form>
     </div>
   )
