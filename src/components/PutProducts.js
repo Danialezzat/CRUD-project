9d60129cb86cab05b0978098759058d202362757
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 04:00:57 -0800

    putProduct is made

--- src/components/PutProducts.js
+++ src/components/PutProducts.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const PutProducts = () => {
   return (
-    <div>PutProducts</div>
+    <div>
+      <h3>Edit a Product</h3>
+      <form action="">
+        <input type="number" placeholder='Id'name='id' />
+        <input type="text" placeholder='Title'name='title' />
+        <input type="text" placeholder='Category'name='category' />
+        <input type="text" placeholder='Description'name='descriptio' />
+        <input type="number" placeholder='Price'name='price' />
+        <input type="file" name='file' />
+        <button type='submit'>Edit Product</button>
+      </form>
+    </div>
   )
 }
 
