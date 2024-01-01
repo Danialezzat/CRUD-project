967d7804ebc8f13e776423376add94da841f8f03
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 03:47:42 -0800

    click handler done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import GetProducts from "./components/GetProducts";
+import AddProducts from "./components/AddProducts";
+import PutProducts from "./components/PutProducts";
+import DeleteProduct from "./components/DeleteProduct";
 
 function App() {
+  const [showComponent, setShowComponent] = useState({
+    get: false,
+    add: false,
+    put: false,
+    delete: false,
+  });
+
+  const btnHandler = (event) => {
+    setShowComponent({
+      ...showComponent,
+      [event.target.name]: !showComponent[event.target.name],
+    });
+  };
+
   return (
     <div>
-      <button>Get Products</button>
-      <button>Add Pruducts</button>
-      <button>Edit Products</button>
-      <button>Delete Products</button>
+      <div>
+        <button name="get" onClick={btnHandler}>
+          Get Products
+        </button>
+        <button name="add" onClick={btnHandler}>
+          Add Pruducts
+        </button>
+        <button name="put" onClick={btnHandler}>
+          Edit Products
+        </button>
+        <button name="delete" onClick={btnHandler}>
+          Delete Products
+        </button>
+      </div>
+      {showComponent.get && <GetProducts />}
+      {showComponent.add && <AddProducts />}
+      {showComponent.put && <PutProducts />}
+      {showComponent.delete && <DeleteProduct />}
     </div>
   );
 }
