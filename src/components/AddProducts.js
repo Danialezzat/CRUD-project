5159fb4c4559c400e8a95a56fe66f3ece74d1fc9
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 03:58:47 -0800

    Base of addProduct is made

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const AddProducts = () => {
   return (
-    <div>AddProducts</div>
+    <div>
+      <h3>Add a New Product</h3>
+      <form action="">
+        <input type="text" placeholder='Title'name='title' />
+        <input type="text" placeholder='Category'name='category' />
+        <input type="text" placeholder='Description'name='descriptio' />
+        <input type="number" placeholder='Price'name='price' />
+        <input type="file" name='file' />
+        <button type='submit'>Add Product</button>
+      </form>
+    </div>
   )
 }
 
