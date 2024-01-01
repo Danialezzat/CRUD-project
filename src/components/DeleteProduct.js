54efe70782c6a5ab682e1ca6ebd6b276d4fd3ad7
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 04:03:47 -0800

    deleteProduct is made

--- src/components/DeleteProduct.js
+++ src/components/DeleteProduct.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const DeleteProduct = () => {
   return (
-    <div>DeleteProduct</div>
+    <div>
+      <h3>Delete a Product</h3>
+      <form action="">
+        <input type="number" placeholder='Type Id...'name='id' />
+        <button type='submit'>Delete Product</button>
+      </form>
+    </div>
   )
 }
 
