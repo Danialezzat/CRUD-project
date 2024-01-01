200bf74cab56b1ee872a83a39a751775e084970b
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 07:34:24 -0800

    axios get request func is set

--- src/components/GetProducts.js
+++ src/components/GetProducts.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import ItemProduct from './ItemProduct'
 import Loading from './Loading'
 
 const GetProducts = () => {
+
+
+  useEffect(() => {
+    getAllData()
+  }, []);
+
+  const getAllData =() => {
+    axios.get(`/products`).then(response => console.log(response)).catch(error => console.log(error.message))
+  };
+
+
+
   return (
     <div style={{
       display: 'flex',
