f5e60d195f05bd462a46192ebcd22db6ea997682
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-01 08:21:48 -0800

    setting PUT with axios done

--- src/components/AddProducts.js
+++ src/components/AddProducts.js
@@ -1,6 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
 const AddProducts = () => {
+
+  const [inputValue, setInputValue] = useState({
+    title: '',
+    category: '',
+    description: '',
+    price: '',
+    image: ''
+  })
+
+  let formData = new FormData();
+
+  const onInputChange = (event) => {
+    switch (event.target.name) {
+      case 'title':
+        setInputValue({...inputValue, title: event.target.value});
+        break;
+      case 'category':
+        setInputValue({...inputValue, category: event.target.value});
+        break;
+      case 'description':
+        setInputValue({...inputValue, description: event.target.value});
+        break;
+      case 'price':
+        setInputValue({...inputValue, price: event.target.value});
+        break;
+      case 'file':
+        setInputValue({...inputValue, image: event.target.files[0]});
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+  const postData = (event) => {
+    event.preventDefault();
+    
+    axios.post(`/products` , inputValue).then(response => {
+    }).catch(error =>{
+      
+    })
+  }
+
+
   return (
     <div style={{
       display: 'flex',
@@ -9,12 +54,12 @@ const AddProducts = () => {
       rowGap: '20px'
     }}>
       <h3>Add a New Product</h3>
-      <form action="">
-        <input type="text" placeholder='Title'name='title' />
-        <input type="text" placeholder='Category'name='category' />
-        <input type="text" placeholder='Description'name='descriptio' />
-        <input type="number" placeholder='Price'name='price' />
-        <input type="file" name='file' />
+      <form action="" onSubmit={postData}>
+        <input value={inputValue.title} onChange={onInputChange} type="text" placeholder='Title' name='title' />
+        <input value={inputValue.category} onChange={onInputChange} type="text" placeholder='Category' name='category' />
+        <input value={inputValue.description} onChange={onInputChange} type="text" placeholder='Description' name='description' />
+        <input value={inputValue.price} onChange={onInputChange} type="number" placeholder='Price' name='price' />
+        <input onChange={onInputChange} type="file" name='file' />
         <button type='submit'>Add Product</button>
       </form>
     </div>
